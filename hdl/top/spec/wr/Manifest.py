27f3e721a99f1f1efc680b4836661afbfcf64f86
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2014-01-17 15:25:06 +0100

    hdl/top/spec: migrate to git version of the Gennum core

--- hdl/top/spec/wr/Manifest.py
+++ hdl/top/spec/wr/Manifest.py
@@ -5,6 +5,6 @@ fetchto = "../../../ip_cores"
 modules = {
     "local" : ["../../../rtl", "../../../platform" ],
     "git" : [ "git://ohwr.org/hdl-core-lib/wr-cores.git",
-					    "git://ohwr.org/hdl-core-lib/etherbone-core.git" ],
-    "svn" : [ "http://svn.ohwr.org/gn4124-core/trunk/hdl/gn4124core/rtl" ]
+					    "git://ohwr.org/hdl-core-lib/etherbone-core.git",
+					    "git://ohwr.org/hdl-core-lib/gn4124-core.git" ]
     }
